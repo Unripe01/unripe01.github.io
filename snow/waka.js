b302e794fa087d75c73a3f75a68a4003296d2e1d
Unripe01 <tomato.ripe.dev@gmail.com>
2022-08-18 12:54:36 +0900

    iwai

--- snow/waka.js
+++ snow/waka.js
@@ -1,7 +1,7 @@
 let snowContainer = document.querySelector(".snow-container");
 
 const createSnow = () => {
-  /* 雪の要素を生成したい */
+  /* 祝いの要素を生成したい */
   let snow = document.createElement("span");
   snow.className = "snow";
   snow.innerHTML = "祝"
@@ -9,17 +9,17 @@ const createSnow = () => {
   minSize = 20;
   maxSize = 40;
 
-  /* 雪の大きさをランダムに生成したい */
+  /* 大きさをランダムに生成したい */
   let snowSize = Math.random() * (maxSize - minSize) + minSize;
 
   snow.style.width = snowSize + "px";
   snow.style.height = snowSize + "px";
 
-  /* 雪の降り始めの位置を決定したい */
+  /* 降り始めの位置を決定したい */
   snow.style.left = Math.random() * 100 + "%";
   snowContainer.appendChild(snow);
 
-  /* 10秒後にたまったspanを消したい */
+  /* span掃除 */
   setTimeout(() => {
     snow.remove();
   }, 10000);
