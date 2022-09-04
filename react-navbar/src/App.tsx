06a474f4e49798527eaafe8cfc8f50817e769cc5
Unripe01 <tomato.ripe.dev@gmail.com>
2022-09-04 17:11:45 +0900

    react-navbar

---
+++ react-navbar/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import { SidebarData } from './components/SidebarData';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-navbar/src/components/SidebarIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from "../images/icon.png"
+
+function SidebarIcon() {
+  return (
+    <div className='SidebarIcon'>
+      <img src={Icon} alt="icon"/>
+      <p>I'm developer</p>
+    </div>
+  )
+}
+
+export default SidebarIcon
